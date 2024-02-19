9ef0a2201311fe7cab273f9fb4434cf1a8544a74
exploitenomah <iksploiting@gmail.com>
2024-02-19 06:29:08 +0100

    FIX - Apple auth: Error Boundary fix

--- app/_errorBoundaries/index.js
+++ app/_errorBoundaries/index.js
@@ -1,4 +1,6 @@
-export default class ErrorBoundary extends React.Component {
+import { Component } from "react"
+
+export default class ErrorBoundary extends Component {
   constructor(props) {
     super(props)
     this.state = { hasError: false }
