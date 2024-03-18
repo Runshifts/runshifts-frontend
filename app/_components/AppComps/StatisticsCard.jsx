8b0c717cc5b20f06f47354cef349deb0f013eaaa
exploitenomah <iksploiting@gmail.com>
2024-03-18 12:42:44 +0100

    refactor employee styles - 1"

---
+++ app/_components/AppComps/StatisticsCard.jsx
@@ -0,0 +1,24 @@
+
+
+export default function StatisticsCard({ title, text, icon, mainBg, iconBg }) {
+  return (
+    <div
+      style={{ backgroundColor: mainBg }}
+      className="bg-[#FFEECC] p-[28px] flex flex-wrap justify-center items-center gap-[16px] md:gap-[24px] md:justify-start rounded-lg"
+    >
+      <div
+        style={{ backgroundColor: iconBg }}
+        className="bg-[#FFDD99] p-[11px] rounded-full"
+      >
+        {icon}
+      </div>
+
+      <div className="px-4 text-center md:text-start">
+        <h1 className="text-[24px] leading-[40px] font-[500] py-2 text-info-700">
+          {title}
+        </h1>
+        <p className="text-[12px] leading-[24px] text-info-500">{text}</p>
+      </div>
+    </div>
+  )
+}
--- app/employee/page.jsx
+++ app/employee/page.jsx
@@ -1,31 +1,32 @@
-import React from "react";
-import DateRangePicker from "../_components/AppComps/Datepicker";
-import CalendarScroll from "../_components/EmployeeComps/CalendarScroll";
-import ShiftSwap from "./ShiftSwap";
-import MyActivities from "./MyActivities";
+import React from "react"
+import DateRangePicker from "../_components/AppComps/Datepicker"
+import Calender from "./Calender"
+import ShiftSwap from "./ShiftSwap"
+import MyActivities from "./MyActivities"
+import Heading from "../_components/Headings"
 
 function EmployeeDashboard() {
   return (
-    <section className="p-3 h-screen">
-      <h1 className="text-[#292D32] font-semibold text-xl md:font-bold text-2xl ">
-        Welcome Ottobong
-      </h1>
-
-      <DateRangePicker />
-
-      <div>
-        <CalendarScroll />
+    <section className="md:px-[21px] h-screen">
+      <div className="flex flex-col gap-[24px] pt-6 pb-4">
+        <Heading as="h1">Welcome Ottobong</Heading>
+        <DateRangePicker />
+      </div>
+      <div className="rounded-md shadow-[0px_2px_8px_0px_#0000001F] flex flex-col gap-[8px] p-[8px] md:p-4">
+        <h2 className="text-[12px] md:text-base font-[600] text-[#292D32] leading-[150%]">
+          Calender
+        </h2>
+        <Calender />
       </div>
 
       <div>
         <ShiftSwap />
       </div>
-
-      <div>
+      <div className="rounded-md overflow-hidden shadow-[0px_2px_8px_0px_#0000001F] ">
         <MyActivities />
       </div>
     </section>
-  );
+  )
 }
 
-export default EmployeeDashboard;
+export default EmployeeDashboard
