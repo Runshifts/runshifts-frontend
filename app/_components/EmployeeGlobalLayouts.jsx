1b4a4f8151e677bfecbced9b9596af73a01ddf2e
exploitenomah <iksploiting@gmail.com>
2024-03-28 14:11:11 +0100

    format

--- app/_components/EmployeeGlobalLayouts.jsx
+++ app/_components/EmployeeGlobalLayouts.jsx
@@ -17,10 +17,11 @@ export default function GlobalLayout({ children }) {
       </div>
 
       <div className="relative md:grid grid-cols-12">
-        {/* <div className=""> */}
         <div
           className={`fixed md:sticky z-[1000] top-0 col-span-1 h-screen md:col-span-3 ${
-            isSidebarOpen ? "visible opacity-100" : "invisible opacity-0 md:opacity-100 md:visible"
+            isSidebarOpen
+              ? "visible opacity-100"
+              : "invisible opacity-0 md:opacity-100 md:visible"
           }`}
         >
           <div className="bg-[#fafbfc] pr-2  border-r-[1px] border-r-[#d1d5db] border-r-solid  h-screen fixed md:relative">
