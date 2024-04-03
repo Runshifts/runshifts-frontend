adb4fbc1e36a472d4b0be623739f80ccb738f99f
exploitenomah <iksploiting@gmail.com>
2024-04-03 12:42:08 +0100

    shift notes display --v1

--- app/employee/my-shifts/ShiftNotesSection.jsx
+++ app/employee/my-shifts/ShiftNotesSection.jsx
@@ -1,12 +1,19 @@
 import ShiftNotesForm from "./ShiftNotesForm"
 
-export default function ShiftNotesSection({ shift }) {
+export default function ShiftNotesSection({ shift, notesDisplayStyle }) {
   return (
     <>
       <div className="flex flex-col items-stretch gap-4 w-full">
         <h3 className="modal-subheading">Notes</h3>
+        <div>
+          <ShiftNotes notesDisplayStyle={notesDisplayStyle || "chat"} />
+        </div>
         <ShiftNotesForm shift={shift} />
       </div>
     </>
   )
+}
+
+function ShiftNotes({ notesDisplayStyle = "chat" || "section" }) {
+  return <></>
 }
