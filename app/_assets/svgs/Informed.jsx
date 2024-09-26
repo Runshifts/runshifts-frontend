7cfa72be665c22a8df172bb59622b99a2b31c9ee
exploitenomah <iksploiting@gmail.com>
2024-09-26 09:22:37 +0100

    clear console errors

--- app/_assets/svgs/Informed.jsx
+++ app/_assets/svgs/Informed.jsx
@@ -1,8 +1,20 @@
 export default function InfoIcon(){
     return (
-        <svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect y="0.689941" width="48" height="48" rx="24" fill="#CBF0BC"/>
-        <path d="M30 32.6899V22.6899M24 32.6899V16.6899M18 32.6899V26.6899" stroke="#449522" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+      <svg
+        width="48"
+        height="49"
+        viewBox="0 0 48 49"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect y="0.689941" width="48" height="48" rx="24" fill="#CBF0BC" />
+        <path
+          d="M30 32.6899V22.6899M24 32.6899V16.6899M18 32.6899V26.6899"
+          stroke="#449522"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     )
   }
