bbfff5bdfc565ed72ea84337fb0abb47323ae3d7
efoneli <abbaefoneli@gmail.com>
2024-10-01 09:29:40 +0100

    bug fix

--- app/_components/homepageComps/HomeHeader.jsx
+++ app/_components/homepageComps/HomeHeader.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import runshiftsLogo1 from './runshiftsLogo2.svg';
 import CommonButtons from './CommonButtons';
 import { MdOutlineCancel } from 'react-icons/md';
+import Link from 'next/link'
 
 const menuItems = [
     { label: 'Home', href: '/' },
@@ -28,7 +29,7 @@ export default function Header({ isTransparent = false }) {
 
     useEffect(() => {
         const handleResize = () => {
-            if (window.innerWidth >= 1024) { 
+            if (window.innerWidth >= 1024) {
                 setIsMobileMenuOpen(false);
             }
         };
@@ -45,21 +46,31 @@ export default function Header({ isTransparent = false }) {
     return (
         <div>
             <nav className="text-[#000] relative z-20">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
-                    <a href="/home" className="flex justify-start items-left">
+                <div className="max-w-screen-xl flex items-center justify-between p-4">
+                    <a href="/" className="flex justify-start items-left">
                         <Image src={runshiftsLogo1} className="h-8" height={200} width={204} alt="runshifts Logo" />
                     </a>
-                    <button
-                        type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                        aria-expanded={isMobileMenuOpen}
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
+                    <div className="flex justify-start items-left mr-4">
+                        <div className='block xl:hidden'>
+                        <Link href='/signup?type=for-profit'>
+<button className='bg-[#449522] rounded-lg text-white p-2 whitespace-nowrap text-sm not-italic font-normal leading-6 xl:text-base '>
+Get Started for <span className='font-bold'>free</span>
+</button>
+</Link>
+                        </div>
+
+                        <button
+                            type="button"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                            aria-expanded={isMobileMenuOpen}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                            </svg>
+                        </button>
+                    </div>
 
                     {/* Desktop Menu */}
                     <div className={`w-full xl:block xl:w-auto ${isMobileMenuOpen ? '' : 'hidden'}`} id="navbar-dropdown">
@@ -81,9 +92,11 @@ export default function Header({ isTransparent = false }) {
                                             )}
                                         </div>
                                     ) : (
-                                        <CommonButtons>
-                                            <a href={item.href}>{item.label}</a>
-                                        </CommonButtons>
+                                        <Link href='/signup?type=for-profit'>
+                                        <button className='bg-[#449522] rounded-lg text-white p-2 whitespace-nowrap text-sm not-italic font-normal leading-6 xl:text-base '>
+                                        Get Started for <span className='font-bold'>free</span>
+                                        </button>
+                                        </Link>
                                     )}
                                     {isDropdownOpen && item.dropdown && (
                                         <div className="absolute left-0 top-full mt-2 z-10 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow">
@@ -107,88 +120,92 @@ export default function Header({ isTransparent = false }) {
 
             {/* Mobile Menu Overlay */}
             {isMobileMenuOpen && (
-    <div className="fixed inset-0 z-30 bg-black bg-opacity-75" onClick={handleOverlayClick}>
-        <div 
-            className="absolute top-0 left-0 w-4/5 h-full bg-white opacity-75 p-4 shadow-lg" 
-            onClick={(e) => e.stopPropagation()}
-        >
-            {/* Cancel button in the top-right corner */}
-            <button
-                onClick={() => setIsMobileMenuOpen(false)}
-                className="absolute top-4 right-4 p-2 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none"
-            >
-                <svg
-                    className="w-6 h-6 text-gray-600"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                    />
-                </svg>
-            </button>
+                <div className="fixed inset-0 z-30 bg-black bg-opacity-75" onClick={handleOverlayClick}>
+                    <div
+                        className="absolute top-0 left-0 w-4/5 h-full bg-white p-4 shadow-lg"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        {/* Cancel button in the top-right corner */}
+                        <button
+                            onClick={() => setIsMobileMenuOpen(false)}
+                            className="absolute top-4 right-4 p-2 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none"
+                        >
+                            <svg
+                                className="w-6 h-6 text-gray-600"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
 
-            {/* Mobile Menu */}
-            <nav className="flex flex-col space-y-4">
-                <ul className="text-black">
-                    {menuItems.map((item, index) => (
-                        <li key={index} className={item.dropdown ? 'relative' : ''}>
-                            {!item.isButton ? (
-                                <div className="flex items-center">
-                                    <a href={item.href} className="block py-2 px-3">
-                                        {item.label}
-                                    </a>
-                                    {item.dropdown && (
-                                        <button
-                                            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                                            className="flex items-center justify-between w-full py-2 px-3"
-                                        >
-                                            <svg
-                                                className="w-2.5 h-2.5 ml-2.5"
-                                                aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 10 6"
-                                            >
-                                                <path
-                                                    stroke="currentColor"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    strokeWidth="2"
-                                                    d="m1 1 4 4 4-4"
-                                                />
-                                            </svg>
-                                        </button>
-                                    )}
-                                </div>
-                            ) : (
-                                <CommonButtons>{item.label}</CommonButtons>
-                            )}
-                            {isDropdownOpen && item.dropdown && (
-                                <div className="absolute left-0 top-full mt-2 z-40 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow">
-                                    <ul className="py-2 text-sm text-black">
-                                        {item.dropdown.map((dropdownItem, dropdownIndex) => (
-                                            <li key={dropdownIndex}>
-                                                <a href={dropdownItem.href} className="block px-4 py-2 hover:bg-gray-100">
-                                                    {dropdownItem.label}
+                        {/* Mobile Menu */}
+                        <nav className="flex flex-col space-y-4">
+                            <ul className="text-black">
+                                {menuItems.map((item, index) => (
+                                    <li key={index} className={item.dropdown ? 'relative' : ''}>
+                                        {!item.isButton ? (
+                                            <div className="flex items-center">
+                                                <a href={item.href} className="block py-2 px-3">
+                                                    {item.label}
                                                 </a>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            )}
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </div>
-    </div>
-)}
+                                                {item.dropdown && (
+                                                    <button
+                                                        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                                                        className="flex items-center justify-between w-full py-2 px-3"
+                                                    >
+                                                        <svg
+                                                            className="w-2.5 h-2.5 ml-2.5"
+                                                            aria-hidden="true"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none"
+                                                            viewBox="0 0 10 6"
+                                                        >
+                                                            <path
+                                                                stroke="currentColor"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth="2"
+                                                                d="m1 1 4 4 4-4"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                )}
+                                            </div>
+                                        ) : (
+                                            <Link href='/signup?type=for-profit'>
+<button className='bg-[#449522] rounded-lg text-white p-2 whitespace-nowrap text-sm not-italic font-normal leading-6 xl:text-base '>
+Get Started for <span className='font-bold'>free</span>
+</button>
+</Link>
+                                        )}
+                                        {isDropdownOpen && item.dropdown && (
+                                            <div className="absolute left-0 top-full mt-2 z-40 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow">
+                                                <ul className="py-2 text-sm text-black">
+                                                    {item.dropdown.map((dropdownItem, dropdownIndex) => (
+                                                        <li key={dropdownIndex}>
+                                                            <a href={dropdownItem.href} className="block px-4 py-2 hover:bg-gray-100">
+                                                                {dropdownItem.label}
+                                                            </a>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            )}
 
         </div>
     );
